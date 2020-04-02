4e7f71cd4a798156383d9af8492928b4b7bcbaa3
iwearthecrowns <iwearthecrowns@users.noreply.github.com>
2020-04-02 09:38:40 -0400

    added sprite 2 and dialogue

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+scene.setBackgroundColor(11)
 let mySprite = sprites.create(img`
 . . . . . . . . . . . . . . . . 
 . . f f f f f f f f f f f . . . 
@@ -12,8 +13,31 @@ f f d d d d d d d d d d d f f .
 . . d d d f f f f f d d d . . . 
 . . . d d d d d d d d d . . . . 
 . . . . d d d d d d d . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
+. . . . 2 2 2 f 2 2 2 . . . . . 
+. . . . 2 2 2 f 2 2 2 . . . . . 
 . . . . . . . . . . . . . . . . 
 `, SpriteKind.Player)
-scene.setBackgroundColor(11)
+let mySprite2 = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . f f f f f f f f f f f . . . 
+. . f . 4 4 e e e 4 4 . f . . . 
+. f . 4 e e e e e e e 4 . f . . 
+. f e e e e e e e e e e e f . . 
+f f e e e e e e e e e e e f f . 
+f f e e e f e e e f e e e f f . 
+f f e e e e e e e e e e e f f . 
+. f e e e e e e e e e e e f . . 
+. . e e f e e e e e f e e . . . 
+. . e e e f f f f f e e e . . . 
+. . . e e e e e e e e e . . . . 
+. . . . e e e e e e e . . . . . 
+. . . . 8 8 8 f 8 8 8 . . . . . 
+. . . . 8 8 8 f 8 8 8 . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Player)
+mySprite2.setPosition(100, 60)
+mySprite.say("eh", 2000)
+pause(2000)
+mySprite2.say("hello", 2000)
+pause(2000)
+mySprite.say("rad")
