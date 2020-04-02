28f906bd01f15dc9d457f5283563bb5e8edee8af
iwearthecrowns <iwearthecrowns@users.noreply.github.com>
2020-04-02 09:23:07 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+let mySprite = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . f f f f f f f f f f f . . . 
+. . f . e e d d d e e . f . . . 
+. f . e d d d d d d d e . f . . 
+. f d d d d d d d d d d d f . . 
+f f d d d d d d d d d d d f f . 
+f f d d d f d d d f d d d f f . 
+f f d d d d d d d d d d d f f . 
+. f d d d d d d d d d d d f . . 
+. . d d f d d d d d f d d . . . 
+. . d d d f f f f f d d d . . . 
+. . . d d d d d d d d d . . . . 
+. . . . d d d d d d d . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Player)
+scene.setBackgroundColor(11)
